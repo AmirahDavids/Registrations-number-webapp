11ba92ae118f0016125f39f7d5827c3e71ec70d4
AmirahDavids <amirahdavids2503@gmail.com>
2020-10-08 16:48:05 +0200

    change connection string for test in travis file

--- reg.sql
+++ reg.sql
@@ -11,8 +11,8 @@ create table vehicles (
     place_id int not null,
     foreign key (place_id) references places(id)
 );
--- delete from places;
--- insert into places (name_of_place, code) values ('Cape Town', 'CA');
--- insert into places (name_of_place, code) values ('Bellville', 'CY');
--- insert into places (name_of_place, code) values ('Stellenbosh', 'CL');
--- insert into places (name_of_place, code) values ('Paarl', 'CJ');
+delete from places;
+insert into places (name_of_place, code) values ('Cape Town', 'CA');
+insert into places (name_of_place, code) values ('Bellville', 'CY');
+insert into places (name_of_place, code) values ('Stellenbosh', 'CL');
+insert into places (name_of_place, code) values ('Paarl', 'CJ');
